ac0f3eef71463a14550e089176b82655a1dfa194
Makinde1034 <makinde1034@gmail.com>
2022-05-06 03:04:20 +0100

    add toast msgs

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -7,7 +7,7 @@ export const PigsV2BusdLP = "0x128179CD02075598CbE0ce3ed6e0673BF7e0326D"
 
 
 export const PIGSAddress = '0x8c48355548890c0Cb973aCbBCf4ce5b5B8c85b66' // TODO: Change address
-export const PigsCreditAddress = '0x6D490044dC1CA783A22cE1eEb1E4443fa16A961c' // TODO: Change address
+export const PigsCreditAddress = '0x3c5eC7698538a74C15Ca01C6cbb5FD6cbbae43AC' // TODO: Change address
 export const PigPenAddress = '0x903754f70dB7090e26d949138D361b1c8B5f9Feb' // TODO: Change address
 export const PiggyBankAddress = '0xa333B5651c2F0a991A9741E458f5D02980d11760' // TODO: Change address
 export const LARGE_NUMBER = '115792089237316195423570985008687907853269984665640564039457584007913129639935'
