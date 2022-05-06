9a7c92ea1a0cb5136a394b2c3eedcfe5ec8359d9
CryptSans <cryptsans@protonmail.com>
2022-05-06 12:16:46 +0100

    Test Launch Deployment

--- src/config/constants/animalFarmToken.ts
+++ src/config/constants/animalFarmToken.ts
@@ -1,10 +1,10 @@
 export const AnimalFarmTokens = {
 	pigsToken: {
-		address: '0x8c48355548890c0Cb973aCbBCf4ce5b5B8c85b66',
+		address: '0x58cD6E0279F83e52DBc114af69ddEa831a50F019',
 		name: 'PIGS',
 		symbol: 'PIGS',
 		decimals: 18,
-		BUSD_LP: '0x128179CD02075598CbE0ce3ed6e0673BF7e0326D',
+		BUSD_LP: '0x89330213A6bA9CC90A449e689240234CD1D6c8db',
 		WBNB_LP: '',
 	},
 }
