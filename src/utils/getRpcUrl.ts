87a12318f21c0669ac07dec0844e7a80fddaa641
CryptSans <cryptsans@protonmail.com>
2022-05-13 00:53:28 +0100

    edit RPC urls

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -2,7 +2,7 @@ import sample from 'lodash/sample'
 
 export const CHAIN_ID = 56
 export const NODES = {
-	56: ['https://bsc-dataseed.binance.org', 'https://bsc-dataseed1.defibit.io', 'https://speedy-nodes-nyc.moralis.io/480f9fd047ed15e4229e4547/bsc/mainnet/archive'],
+	56: ['https://bsc-dataseed.binance.org', 'https://bsc-dataseed1.defibit.io'],
 	97: [
 		'https://speedy-nodes-nyc.moralis.io/480f9fd047ed15e4229e4547/bsc/testnet/archive',
 		'https://speedy-nodes-nyc.moralis.io/480f9fd047ed15e4229e4547/bsc/testnet/archive',
