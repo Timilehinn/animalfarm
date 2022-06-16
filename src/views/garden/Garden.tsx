4547ac15e44d9697a3510fc716af53e1f576c317
Makinde1034 <makinde1034@gmail.com>
2022-06-16 11:35:27 +0100

    chore: drip garden

--- src/views/garden/Garden.tsx
+++ src/views/garden/Garden.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import BigNumber from 'bignumber.js'
 
 import Info from 'components/Info/Info'
+import PigsCreditCard from 'components/PigsCreditCard/PigsCreditCard'
 
 import styles from './Garden.module.scss'
 import pigs from '../../assets/svgg.png'
@@ -28,13 +29,20 @@ function Garden() {
 	return (
 		<div className={styles.addliquidity__wrap}>
 			<div className={styles.addliquidity}>
+				<div className={styles.cards}>
+					<PigsCreditCard title='Total LP locked' amount='0 DRIP/BUSD' />
+		
+						<PigsCreditCard title='Total value of LP locked' amount='$0' />
+				
+				</div>
+
 				<section>
 					<header>Buy plants with LP tokens</header>
 					{/* <p className={styles.info}>Enter the amount of BUSD to be paired with PIG</p> */}
 					<div className={styles.info__area}>
-                        <div className={styles.bonus} >
-                            <div className={styles.bonus__in}>Ferterlizers bonus</div>
-                        </div>
+						<div className={styles.bonus}>
+							<div className={styles.bonus__in}>Ferterlizers bonus</div>
+						</div>
 						<Info title='Time' info='0' />
 						<Info title='Balance' info='0' />
 						<Info title='Total' info='0' />
@@ -42,19 +50,20 @@ function Garden() {
 					{/* input 1 */}
 					<div className={styles.inputBox}>
 						<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-							<div onClick={()=>setIsMenuOpen(!isMenuOpen)} className={styles.logo}>
+							<div onClick={() => setIsMenuOpen(!isMenuOpen)} className={styles.logo}>
 								<img src={busd} alt='' />
-                                <img id={styles.drip} src={drip} alt="" />
+								<img id={styles.drip} src={drip} alt='' />
 								<p>DRIP/BUSD LP</p>
 							</div>
 							<input min='0' required type='number' placeholder='0.0' />
 						</div>
-						
 					</div>
 					{/* input 2 */}
-					<button type='button' className={styles.button__disabled}>Deposit</button>
+					<button type='button' className={styles.button__disabled}>
+						Deposit
+					</button>
 					<div className={styles.reward__center}>
-						<p className={styles.reward__header} >Reward center</p>
+						<p className={styles.reward__header}>Reward center</p>
 						<div className={styles.info__area}>
 							<Info title='Plants grown' info='0' />
 							<Info title='Seeds available' info='0' />
