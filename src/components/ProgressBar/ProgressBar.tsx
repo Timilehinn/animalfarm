286b638f05e6235800904fa71ae31d85e16b8ca8
CryptSans <cryptsans@protonmail.com>
2022-06-20 17:17:03 +0100

    updates: Changes to Progress bar

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import './ProgressBar.scss'
 
-interface progressBarProps{
-    progress : string
+interface progressBarProps {
+	progress: string
 }
 
-function ProgressBarr({progress}:progressBarProps) {
-    
+function ProgressBarr({ progress }: progressBarProps) {
 	return (
-        <div className='bar'>
-            <div style={{width:`${progress}`}} className='bar__in' >{}</div>
-        </div>
-    )
+		<div className='bar'>
+			<div style={{ width: `${progress}%` }} className='bar__in'>
+				{}
+			</div>
+		</div>
+	)
 }
 
 export default ProgressBarr
