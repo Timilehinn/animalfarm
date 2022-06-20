a73df7413b2c2f4d2f6a8dd5444de7dae7c87743
CryptSans <cryptsans@protonmail.com>
2022-06-20 16:53:06 +0100

    fix: Typo Fertilizers

--- src/views/garden/Garden.tsx
+++ src/views/garden/Garden.tsx
@@ -347,7 +347,7 @@ function Garden() {
 
 					<div className={styles.info__area}>
 						<div className={styles.bonus}>
-							<div className={styles.bonus__in}>Ferterlizers Bonus</div>
+							<div className={styles.bonus__in}>Fertilizers Bonus</div>
 						</div>
 						<Info title='Time' info={timeMultiplierText} />
 						<Info title='Balance' info={`${balanceMultiplierText}%`} />
